def5c3298051587046b9f79f3e7d97f842be15a6
xc21 <xc21@rice.edu>
2017-06-28 16:27:46 +0800

    Update Final Project.R

--- Final Project.R
+++ Final Project.R
@@ -1,3 +1,4 @@
+library(plyr)
 library(dplyr)
 library(tidyverse)#library for filter
 library(ggplot2)
@@ -14,6 +15,8 @@ flood<-flood[-21]
 flood$Began<-as.Date(as.character((flood$Began)), format = " %d-%h-%y")
 flood$Ended<-as.Date(as.character((flood$Ended)), format = " %d-%h-%y")
 flood$Duration.in.Days<-as.numeric(flood$Duration.in.Days)
+
+
 for (i in 1:nrow(flood)){
   if(!is.na(flood$Began[i])){
     if(format.Date(flood$Began[i], "%m")=="12" || format.Date(flood$Began[i],"%m")=="01"|| format.Date(flood$Began[i],"%m")=="02"){
@@ -45,45 +48,111 @@ for (i in 1:nrow(flood)){
 
 ###Q1:
 #flood seasonality and duration
-ggplot(flood,aes(x=Season,fill=Duration.length))+ geom_bar() + coord_flip() + labs(title="Flood seasonality and their duration") 
+ggplot(flood,aes(x=Season,fill=Duration.length))+ geom_bar() +coord_flip() + labs(title="flood seasonality and duration")+ theme_bw()
 
-###Q2:
-###What are the main causes of these floods?
+  library(dplyr)
+library(ggplot2)
+library(ggmap)
+library(maps)
+library(stringr)
+library(data.table)
+knitr::opts_chunk$set(echo = TRUE)
 
-#create a df or something else with the summary output.
-temp <- row.names(as.data.frame(summary(flood$Main.cause, max=10))) 
-flood$Main.cause<- as.character(flood$Main.cause) 
+#import data
+#setwd("/Users/sadierichardson/Desktop")
+read.csv("C:/Users/sony/Desktop/STAT 605/Final Project/Project/flood.csv") -> flood
+flood1 <- flood
+
+#Separate data into groups by region (using latitude and longitude)
+#rename centroidx and centroidy as latitude and longitude
+colnames(flood1)[colnames(flood1) == "Centroid.Y"] <- "latitude"
+colnames(flood1)[colnames(flood1) == "Centroid.X"] <- "longitude"
+#lat and long in numeric format
+flood1$latitude <- as.numeric(as.character(flood1$latitude))
+flood1$longitude <- as.numeric(as.character(flood1$longitude))
+  
+#make a world map
+map.world <- map_data(map="world")
+map <- ggplot()
+map <- map + geom_map(data=map.world, map=map.world, aes(map_id=region, x=long, y=lat), fill = "white", col = "gray45")
+ 
+ #format begin as date
+flood1$Began<-as.Date(as.character((flood1$Began)), format = " %d-%h-%y")
+#format month as character
+flood1$month <- as.character(format(flood1$Began, format = '%h'))
+#add season column
+flood1$season <-  ifelse(flood1$month %in% c("Jun", "Jul", "Aug"),"Summer", 
+                         ifelse(flood1$month %in% c("Dec", "Jan", "Feb"), "Winter", 
+                                ifelse(flood1$month %in% c("Sep", "Oct", "Nov"), "Fall", 
+                                       ifelse(flood1$month %in% c("Mar", "Apr", "May"), "Spring", NA))))
+flood_season <- flood1 %>%
+  filter(!is.na(season))
+  
+ season_map <- map +
+  geom_point(data = subset(flood1, season %in% c("Fall", "Spring", "Summer", "Winter")), aes(x=longitude, y=latitude, color = season, size = quartile), size = .5, alpha = .2) + labs(title = "Floods Around the World by Season", x = "Longitude", y = "Latitude")  +  facet_wrap(~ season, nrow = 2) + theme(legend.position="none")
+  
+season_map
+  
+  flood1$Year<-substring(flood1$Began,1,4) 
+occ<-count(flood1,'Year')
+ggplot(flood1, aes(Began, fill = Severity..)) + geom_histogram() +  scale_x_date()+geom_bar(width=1)+  theme_bw()+labs(title="Severity Across Years")+xlab("flood Began Date")+ theme_bw()
+  
+  flood$Began<-as.Date(as.character((flood$Began)), format = " %d-%h-%y")
+flood$Ended<-as.Date(as.character((flood$Ended)), format = " %d-%h-%y")
+flood$Duration.in.Days<-as.numeric(flood$Duration.in.Days)
 
 
-#pull out top 10 most frequence causes, and list other not that frequent causes as "other"
+for (i in 1:nrow(flood)){
+  if(!is.na(flood$Began[i])){
+    if(format.Date(flood$Began[i], "%m")=="12" || format.Date(flood$Began[i],"%m")=="01"|| format.Date(flood$Began[i],"%m")=="02"){
+      flood$Season[i]<-"Winter"
+    } else if(format.Date(flood$Began[i], "%m")=="03" || format.Date(flood$Began[i], "%m")=="04"|| format.Date(flood$Began[i], "%m")=="05"){
+      flood$Season[i]<-"Spring"
+    } else if (format.Date(flood$Began[i], "%m")=="06" || format.Date(flood$Began[i], "%m")=="07"|| format.Date(flood$Began[i], "%m")=="08"){
+      flood$Season[i]<-"Summer"
+    } else {
+      flood$Season[i]<-"Autumn"
+    }
+  }
+  if(flood$Duration.in.Days[i]<=7 && flood$Duration.in.Days[i]>=1){
+    flood$Duration.length[i]<-"S" #stands for short
+    
+  } else if(flood$Duration.in.Days[i]>=7 && flood$Duration.in.Days[i]<=21){
+    flood$Duration.length[i]<-"M" #stands for medium
+    
+    
+  } else if(flood$Duration.in.Days[i]>=21 && flood$Duration.in.Days[i]<=35 ){
+    flood$Duration.length[i]<-"L" #stands for long
+    
+  } else{
+    flood$Duration.length[i]<-"VL" #stands for very long
+    
+  }
+}
+flood_noNA<- flood[!(flood$Severity..=="#N/A"),]
+ggplot(flood_noNA) + aes(x = Severity.., y = Duration.length) + geom_count() + labs(title="Flood Severity v.s Duration Length")+ theme_bw()
+  
+  flood_SVL<- subset(flood, flood$Severity.. ==2 & flood$Duration.length=="VL")
 
-flood$TopCause <- ifelse(
+temp2 <- row.names(as.data.frame(summary(flood_SVL$Country, max=8))) 
+temp3 <- row.names(as.data.frame(summary(flood$Country, max=8))) 
+flood_SVL$Country<-as.character(flood_SVL$Country)
+
+flood_SVL$TopCountry <- ifelse(
   #condition: match flood$Main.cause with row.names in summary data frame
-  flood$Main.cause %in% temp, 
+  flood_SVL$Country %in% temp2, 
   # if satisfies the condition, then it should be named as flood$Main.cause
-  flood$Main.cause, 
+  flood_SVL$Country, 
   ## else it should be named "Other"
   "Other" 
 )
 #factorize the output
-flood$TopCause<- as.factor(flood$TopCause)
-Top_Causes<- factor(flood$TopCause,
-                    levels=names(sort(table(flood$TopCause),increasing=TRUE)))
+flood_SVL$TopCountry<- as.factor(flood_SVL$TopCountry)
+flood_SVL_temp<-subset(flood_SVL,!TopCountry=="Other")
 
-ggplot(flood,aes(x=Top_Causes))+ geom_bar() + coord_flip() + labs(title="What are the main causes of these floods?") 
+#the pie chart with "other" excluded(theme different!!)
+library(grid)
+library(gridBase)
 
-#
-subset(flood, format.Date(flood$Began, "%m")=="05" & format.Date(date, "%y")=="07")
-
-levels(flood$country)
-unique(flood$Country) #346 countries, top countries: USA, China, India, Indonesia,Philippines,
-summary(flood)
-
-#Q2:seasonality analysis
-ggplot(flood, aes(x=Season, y=Duration.in.Days)) + 
-  geom_violin()
-
-
-#flood in usa
-flood_USA<-subset(flood,Country=="USA")
-
+bp<- ggplot(flood_SVL_temp, aes(x=factor(TopCountry,levels=names(sort(table(TopCountry),increasing=TRUE))), y="", fill=TopCountry))+geom_bar(width = 1,stat = "identity")
+pie <- bp + coord_polar("y") +ylab("")+xlab("")+labs(title="Long & Severe floods Among Countries")+ theme_minimal()+theme(axis.text = element_blank(), legend.position="left")
